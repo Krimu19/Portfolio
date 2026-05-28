f8d7c6744759d08d7b4362385e8c9a7c34e329cf
Krimu19 <Krimu.bouzidi7@gmail.com>
2026-05-28 09:48:10 +0200

    change

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-const timeline = document.getElementById("timeline");
+AOS.init();
 
-/* aller à droite */
-function scrollRightTimeline() {
-    timeline.scrollBy({
-        left: 400,
-        behavior: "smooth"
-    });
-}
 
-/* revenir à gauche */
-function scrollLeftTimeline() {
-    timeline.scrollBy({
-        left: -400,
-        behavior: "smooth"
-    });
-}
 
-/* scroll automatique avec la molette */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* scroll automatique avec la molette 
 function scrollTimeline(event) {
     event.preventDefault();
 
     timeline.scrollLeft += event.deltaY;
 }
 
-timeline.addEventListener("wheel", scrollTimeline);
+timeline.addEventListener("wheel", scrollTimeline);*/
